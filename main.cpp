bb935812cb26a383247a958280b0d729d3f15723
AlbertosPo <albertospoponidis@gmail.com>
2025-08-05 18:13:24 +0300

    mark line 14 important

--- main.cpp
+++ main.cpp
@@ -12,5 +12,5 @@ int main()
 	std::cout << "Hello world!\n";
 	std::cout << "Hello world!\n";
 	std::cout << "Hello world! I love this line\n";
-	std::cout << "Hello world!\n";
+	std::cout << "Hello world! - super cool line, needed to have it\n";
 }
