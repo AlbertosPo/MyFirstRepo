08174f92d9a2d740ad56ae59ceadb8d6d838f622
AlbertosPo <albertospoponidis@gmail.com>
2025-08-05 20:52:25 +0300

    removed line 15

--- main.cpp
+++ main.cpp
@@ -12,6 +12,6 @@ int main()
 	std::cout << "Hello world!\n";
 	std::cout << "Hello world!\n";
 	std::cout << "Hello world! I love this line\n";
-	std::cout << "Hello world! - super cool line, needed to have it\n";
+
 	std::cout << "I added a new one" << std::endl;
 }
