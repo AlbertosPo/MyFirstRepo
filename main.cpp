8c7c3a000cf2b247d5268eba3a40972e6b66c1a9
AlbertosPo <albertospoponidis@gmail.com>
2025-08-05 20:44:45 +0300

    added another line

--- main.cpp
+++ main.cpp
@@ -13,4 +13,5 @@ int main()
 	std::cout << "Hello world!\n";
 	std::cout << "Hello world! I love this line\n";
 	std::cout << "Hello world! - super cool line, needed to have it\n";
+	std::cout << "I added a new one" << std::endl;
 }
